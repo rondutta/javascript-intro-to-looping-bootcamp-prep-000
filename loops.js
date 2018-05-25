87a1052e2ee22800062702dfd5978809653948c0
Rayan Dutta <rondutta23@gmail.com>
2018-05-25 07:04:10 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,26 @@
-var forLoop = function(array){
-  for(let i=0;i<25;i++){
-    if(i===1){
-      array+= "I am 1 strange loop";
-    }
-    else
-    array += `I am ${i} strange loop.`;
+function forLoop(array) {
+  for (var i = 0; i < 25; i++) {
+    array.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`);
   }
-};
+  return array;
+}
+
+function whileLoop(n) {
+  var i = n;
+  while (i > 0) {
+    console.log(i);
+    i--;
+  }
+  return "done";
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array) {
+  do {
+    array.splice(1);
+  } while (maybeTrue());
+  return array;
+}
